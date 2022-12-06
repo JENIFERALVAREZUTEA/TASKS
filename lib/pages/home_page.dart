660de7dc1574fd7d82b87db38653e0fddc8c740c
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-12-06 14:49:27 -0500

    VIDEO 6 OBTENER INFORMACION DESDE FIRESTORE

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,47 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+CollectionReference tasksReference =FirebaseFirestore.instance.collection('tasks');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Firebase Firestore"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: (){
+              tasksReference.get().then((value){
+               // QuerySnapshot collection = value;
+                //List<QueryDocumentSnapshot> docs = collection.docs;
+                //QueryDocumentSnapshot doc = docs[0];
+               // print(doc.id);
+               // print(doc.data());
+               QuerySnapshot collection = value;
+               collection.docs.forEach((element) {
+               // print(element.data());
+               Map<String,dynamic> myMap = element.data() as Map<String,dynamic>;
+               print(myMap["nombre"]);
+               });
+              }
+              );
+              },
+              child: Text(
+                "Obtener la data"
+              ),
+            
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
