db96c93c8f4e52960d9ec2c1156ee08c3ee543a5
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-12-06 18:27:45 -0500

    video 9 introduccion a streams

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,124 +1,47 @@
 
 
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-CollectionReference tasksReference =FirebaseFirestore.instance.collection('tasks');
+CollectionReference tasksReference =
+FirebaseFirestore.instance.collection('tasks');
+
+Stream<int> counter() async*{
+  for (int i = 0 ;i <10; i++){
+    yield i;
+    await Future.delayed( const Duration(seconds: 2));
+  }
+}
+
+Future <int> getNumber()async{
+  return 1000;
+}
 
   @override
   Widget build(BuildContext context) {
+
+    counter().listen((event){
+      print(event);
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Firebase Firestore"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: (){
-              tasksReference.get().then((value){
-               // QuerySnapshot collection = value;
-                //List<QueryDocumentSnapshot> docs = collection.docs;
-                //QueryDocumentSnapshot doc = docs[0];
-               // print(doc.id);
-               // print(doc.data());
-               QuerySnapshot collection = value;
-               collection.docs.forEach((element) {
-               // print(element.data());
-               Map<String,dynamic> myMap = element.data() as Map<String,dynamic>;
-               print(myMap["nombre"]);
-               });
-              }
-              );
-              },
-              child: Text(
-                "Obtener la data"
-              ),
-            
-              ),
-
-            ElevatedButton(
-              onPressed: () {
-              
-              tasksReference.add(
-                {
-                "nombre": "Marleni",
-                "Apellidos": "Ayma Letona",
-
-              },
-              ).then((DocumentReference value) {
-                print(value.id);
-              }).catchError ((error){
-                print("Ocurrio un error en el registro");
-              }).whenComplete((){
-                print("El registro a terminado");
-              });
-            },
-            child: Text(
-              "Agregar documento",
-            ),
-            ),
-
-            ElevatedButton(onPressed: (){
-              tasksReference
-              .doc("V9pmY0sCZ1ZG1QiALxJS")
-              .update(
-                {
-                  "nombre":"Luz Maria",
-                  "Apellidos":"Quispe Achahui",
-                },
-                )
-              .catchError((error){
-                print(error);
-              },
-              ).whenComplete(
-                () {
-                print("Actualizacion terminada");
-                },
-              );
-            }, 
-            child: Text(
-              "Actualizar Documento"
-              ),
-              ),
-          
-            ElevatedButton(onPressed: (){
-              
-              tasksReference.doc("V9pmY0sCZ1ZG1QiALxJS").delete().catchError(
-                (error){
-                  print(error);
-                },
-              ).whenComplete((){
-                print("La eliminacion esta completada");
-              },
-              );
-            }, 
-            child: Text(
-              "Eliminar documento"
-              )
-              ),
-         
-            ElevatedButton(
-              onPressed: (){
-              tasksReference.doc("A0001").set(
-                {
-                  "nombre":"Paloma",
-                  "Apellidos":"Portilla",
-                },
-                ).catchError((error)
-                {
-                  print("Creacion completada");
-                });
-            },
-             child: Text("Agregar documento personalizado"
-             )
-             )
-          ],
-        ),
-      ),
+     body: StreamBuilder(
+      stream: counter(),
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        if(snapshot.hasData){
+          int data = snapshot.data;
+         return Center(child: Text(data.toString(),style: TextStyle(fontSize: 50),),);
+        }
+        return Center(child: CircularProgressIndicator(),);
+      },
+     ),
     );
   }
 }
