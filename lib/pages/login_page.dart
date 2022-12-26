e4838024579bfa63218b4e1b0fbcf975eca0a2b6
JENIFERALVAREZUTEA <202000202d@utea.edu.pe>
2022-12-26 09:08:34 -0500

    video 34 login google 2

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,8 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:tasks/models/user_model.dart';
 import 'package:tasks/pages/home_page.dart';
 import 'package:tasks/pages/register_page.dart';
+import 'package:tasks/services/my_service_firestore.dart';
 import 'package:tasks/ui/general/colors.dart';
 import 'package:tasks/ui/widgets/button_custom_widget.dart';
 import 'package:tasks/ui/widgets/button_normal_widget.dart';
@@ -22,42 +24,38 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _passwordController = TextEditingController();
   final GoogleSignIn _googleSigIn = GoogleSignIn(scopes: ["email"]);
 
-  _login() async {
-    try {
-      if (formKey.currentState!.validate()) {
-        UserCredential userCredential = await FirebaseAuth.instance
-            .signInWithEmailAndPassword(
-                email: _emailController.text,
-                password: _passwordController.text);
+  MyServiceFirestore userService = MyServiceFirestore(collection: "users");
 
-        if (userCredential.user != null) {
-          Navigator.pushAndRemoveUntil(
-              context,
-              MaterialPageRoute(builder: (context) => HomePage()),
-              (route) => false);
-        }
+
+  //Login normal
+  _login() async{
+    try{
+      if(formKey.currentState!.validate()){
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _emailController.text, 
+        password: _passwordController.text);
+      if(userCredential.user != null){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=>HomePage()), (route) => false);
       }
-    } on FirebaseAuthException catch (error) {
-      if (error.code == "invalid-email") {
+      }
+    } on FirebaseAuthException catch(error){
+      if(error.code == "invalid-email"){
         showSnackBarError(context, "El correo electrónico es inválido");
-      } else if (error.code == "user-not-found") {
+      }else if(error.code == "user-not-found"){
         showSnackBarError(context, "El usuario no está registrado");
-      } else if (error.code == "wrong-password") {
+      }else if(error.code == "wrong-password"){
         showSnackBarError(context, "Contraseña incorrecta");
       }
     }
   }
-
-    //Login con google
+  //Login con google
   _loginWithGoogle() async{
    GoogleSignInAccount? googleSignInAccount = await _googleSigIn.signIn();
-
+    
     if(googleSignInAccount == null){
       return;
     }
-
     GoogleSignInAuthentication _googleSignInAuth = await googleSignInAccount.authentication;
-
      OAuthCredential credential = GoogleAuthProvider.credential(
       idToken: _googleSignInAuth.idToken ,
       accessToken: _googleSignInAuth.accessToken ,
@@ -65,9 +63,25 @@ class _LoginPageState extends State<LoginPage> {
 
     UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
 
+    if(userCredential.user != null){
+      UserModel userModel = UserModel(
+        fullName: userCredential.user!.displayName!, 
+        email: userCredential.user!.email!,
+        );
+        userService.cherckUser(userCredential.user!.email!).then((value){
+          if(!value){
+            userService.addUser(userModel).then((value){
+              if(value.isNotEmpty){
+                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> HomePage()), (route) => false);
+              }
+            });
+          }else {
+                Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> HomePage()), (route) => false);
+          }
+        });
+    }
 
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/services/my_service_firestore.dart
+++ lib/services/my_service_firestore.dart
@@ -1,25 +1,26 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tasks/models/task_model.dart';
 import 'package:tasks/models/user_model.dart';
 
 class MyServiceFirestore {
-   String collection;
-   MyServiceFirestore({required this.collection});
+  String collection;
+  MyServiceFirestore({required this.collection});
 
-  late CollectionReference _collectionReference = FirebaseFirestore.instance.collection(collection);
+  late CollectionReference _collectionReference =
+      FirebaseFirestore.instance.collection(collection);
 
-//añadir 
-Future <String> addTask(TaskModel model)async{
-  DocumentReference documentReference = await _collectionReference.add(model.toJson());
-  String id = documentReference.id;
-  return id;
-}
-//borrar 
+//añadir
+  Future<String> addTask(TaskModel model) async {
+    DocumentReference documentReference =
+        await _collectionReference.add(model.toJson());
+    String id = documentReference.id;
+    return id;
+  }
+//borrar
 
-  Future <void> finishiedTask(String taskId) async{
+  Future<void> finishiedTask(String taskId) async {
     await _collectionReference.doc(taskId).update(
       {
         "status": false,
@@ -27,10 +28,23 @@ Future <String> addTask(TaskModel model)async{
     );
   }
 
-    Future<String> addUser(UserModel userModel)async{
-    DocumentReference documentReference = await _collectionReference.add(userModel.toJson());
+  Future<String> addUser(UserModel userModel) async {
+    DocumentReference documentReference =
+        await _collectionReference.add(userModel.toJson());
     return documentReference.id;
   }
+
+ //verificar usuario
+
+  Future<bool> cherckUser(String email) async{
+   QuerySnapshot collection = await _collectionReference.where("email", isEqualTo: email).get();
+   if(collection.docs.isNotEmpty){
+    return true;
+   } else{
+    return false;
+   }
+  }
+
 
 
 }
